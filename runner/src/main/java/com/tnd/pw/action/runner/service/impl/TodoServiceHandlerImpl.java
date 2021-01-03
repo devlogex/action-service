a27352fac21bab536654d0bfdd3e67d6d68f747a
devlogex <tannd.devlogex@gmail.com>
2021-01-03 22:43:13 +0700

    update

--- runner/src/main/java/com/tnd/pw/action/runner/service/impl/TodoServiceHandlerImpl.java
+++ runner/src/main/java/com/tnd/pw/action/runner/service/impl/TodoServiceHandlerImpl.java
@@ -30,8 +30,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class TodoServiceHandlerImpl implements TodoServiceHandler {
